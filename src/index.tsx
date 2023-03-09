3069d4502df0894a8c0e1f189994604ba858e100
slimemoss <common.tken@gmail.com>
2023-03-09 21:23:04 +0900

    add browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import {P1, P2} from './pages'
 
 ReactDOM.render(
   <>
-    test
+    <BrowserRouter>
+      <Routes>
+        <Route path="/react-router-test/" element={<P1/>} />
+        <Route path="/react-router-test/p1" element={<P1/>} />
+        <Route path="/react-router-test/p2" element={<P2/>} />
+      </Routes>
+    </BrowserRouter>
   </>,
   document.getElementById('app')
 )
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export const P1 = () => {
+  return (
+    <>
+      p1
+    </>
+  )
+}
+
+export const P2 = () => {
+  return (
+    <>
+      p2
+    </>
+  )
+}
