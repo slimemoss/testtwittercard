6ca66dcbbefd3980d030451de177db1c8577c7c2
slimemoss <common.tken@gmail.com>
2023-03-09 21:39:49 +0900

    routerが機能しないので、HashRouterを試す
    https://stackoverflow.com/questions/71984401/react-router-not-working-with-github-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { Route, Routes, HashRouter } from 'react-router-dom'
 import {P1, P2} from './pages'
 
 ReactDOM.render(
   <>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/react-router-test/" element={<P1/>} />
         <Route path="/react-router-test/p1" element={<P1/>} />
         <Route path="/react-router-test/p2" element={<P2/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </>,
   document.getElementById('app')
 )
