2e11e87c9bdcb22112551ce04698b4ff945a7be7
slimemoss <common.tken@gmail.com>
2023-03-13 17:32:05 +0900

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
 // https://qiita.com/teinen_qiita/items/4e828ac30221efb624e1
 
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: 'production',
   output: {
     publicPath: '/react-router-test/#',
   },
